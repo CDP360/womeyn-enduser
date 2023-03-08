ee3d80d6d75e3503c5aaec6644ba14f97a30ca6d
CDP360 <kalai@cdp360.com>
2023-03-09 00:27:36 +0530

    oauthcomplete page bug fixing

--- src/components/oauthcomplete/Oauthcomplete.js
+++ src/components/oauthcomplete/Oauthcomplete.js
@@ -2,23 +2,16 @@ import React, { useEffect } from 'react'
 import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
 import LoaderLogo from '../loaderlogo/LoaderLogo';
-import { OauthSuccess, oAuthSuccessTokenStage } from '../../services/user-login-service/user-login-services';
-import CredentialsProvider from "next-auth/providers/credentials";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { OauthSuccess } from '../../services/user-login-service/user-login-services';
+
 
 function Oauthcomplete() {
-    const { data: session } = useSession();
+
     const history = useRouter();
     const { redirect } = history.query;
     useEffect(() => {
         OauthSuccess().then(async (res) => {
-            // toast.success("Sucess!!!");
             localStorage.setItem("womenUserToken", JSON.stringify(res?.data?.tokens?.access?.token))
-            // const result = await signIn("credentials", {
-            //     redirect: false,
-            //     email: res?.data?.user?.email,
-            //     password: res?.data?.user?.password || null
-            // })
             setTimeout(() => {
                 router.push("/");
             }, 1000)
