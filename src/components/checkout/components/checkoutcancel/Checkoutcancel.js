69e2377ea9aaefc5a34fc59cf6045e74a3b50cf0
CDP360 <kalai@cdp360.com>
2023-03-22 20:10:44 +0530

    checkout cancel screen bug fixing in deploy error

--- src/components/checkout/components/checkoutcancel/Checkoutcancel.js
+++ src/components/checkout/components/checkoutcancel/Checkoutcancel.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import styles from './styles/Cancelorder.module.scss';
 function Checkoutcancel() {
     return (
         <div className={styles.cancelsection}>
 
+            Cancel Order!!!!!!....
 
         </div>
     )
