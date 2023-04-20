2e0a99467ea620998fc6743e95653dfdbefea0f3
CDP360 <kalai@cdp360.com>
2023-04-20 23:41:09 +0530

    product list page in rating added and implemented in loic for star rating

--- src/components/productcategorys/ProductCategorys.js
+++ src/components/productcategorys/ProductCategorys.js
@@ -22,6 +22,8 @@ import LoaderLogo from '../loaderlogo/LoaderLogo';
 import { AllProductCategorys } from '../../services/productview-service/productview-services';
 import star from '../../assests/homepage-logos/stars.png';
 
+import { Rate } from "antd";
+
 
 function ProductCategorys() {
     const router = useRouter();
@@ -33,22 +35,22 @@ function ProductCategorys() {
     const [filters, setFilterData] = useState([]);
     const [filterdata, setFilter] = useState("");
     const [searchname, setSearchName] = useState('');
-  
+
     const [categoryid, setCategoryId] = useState(0);
     const [error, setError] = useState(false);
-    const [loading,setLoading]=useState(false);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
         setLoading(true);
         AllProductCategorys().then((res) => {
             setServiceusers(res?.data?.results);
 
-            setTimeout(()=>{
-        setLoading(false);
+            setTimeout(() => {
+                setLoading(false);
 
-            },500)
+            }, 500)
         }).catch((err) => {
             console.log(err);
-        setLoading(false);
+            setLoading(false);
 
         })
 
@@ -132,6 +134,8 @@ function ProductCategorys() {
         return originalElement;
     }
 
+    console.log(servicesusers, "servicesusers")
+
     return (
         <Fragment>
             <div className='mainsection'>
@@ -177,54 +181,63 @@ function ProductCategorys() {
 
 
                             <div className='cardsection row mb-3 ms-1 mt-5'>
-                    {loading ? <>
-                        <LoaderLogo />
-                    </> : <>{servicesusers?.map((item, index) => {
-                        return (
-                            <div className='card col-lg-3 col-sm-6 col-xs-6 col-md-10 ' key={index} >
-                                {/* <div className={styles.plussection}>
+                                {loading ? <>
+                                    <LoaderLogo />
+                                </> : <>{servicesusers?.map((item, index) => {
+                                    return (
+                                        <div className='card col-lg-3 col-sm-6 col-xs-6 col-md-10 ' key={index} >
+                                            {/* <div className={styles.plussection}>
                                     <ion-icon name="add-outline" className={styles.ionicicons} onClick={() => Carthandleproduct(item)}></ion-icon>
                                     <Image src={plus} alt="no image" className={styles.plus} onClick={() => Carthandleproduct(item)} />
                                 </div> */}
-                                <div onClick={() => router.push(`/product/${item?.productSlugName}`)} className={styles.imagebox}>
-                                    {item?.productThumbImage ? <img src={`https://my-demo-11-bucket.s3.ap-south-1.amazonaws.com/${item?.productThumbImage}`} alt="no image" className={styles.sellerimagesize} /> :
-                                        <>
-                                            <Skeleton className={styles.loaderskelimage} />
-                                        </>
-                                    }
-                                </div>
-                                <div className={styles.cardinsidesection} onClick={() => router.push(`/product/${item?.productSlugName}`)}>
-                                    <Image src={star} alt="no image" className={styles.stars} />
-                                    <div className={styles.brandname}>
-                                        {item?.productName?.length <= 10 ? <>{item?.productName}</> : <>    {item?.productName.slice(0, 18)}...</>}
-                                    </div>
+                                            <div onClick={() => router.push(`/product/${item?.productSlugName}`)} className={styles.imagebox}>
+                                                {item?.productThumbImage ? <img src={`https://my-demo-11-bucket.s3.ap-south-1.amazonaws.com/${item?.productThumbImage}`} alt="no image" className={styles.sellerimagesize} /> :
+                                                    <>
+                                                        <Skeleton className={styles.loaderskelimage} />
+                                                    </>
+                                                }
+                                            </div>
+                                            <div className={styles.cardinsidesection} onClick={() => router.push(`/product/${item?.productSlugName}`)}>
+                                                {/* <Image src={star} alt="no image" className={styles.stars} /> */}
 
-                                    <div className='textgrey'>
-                                        {item?.brandName?.length <= 18 ? <>{item?.brandName}</> : <>{item?.brandName.slice(0, 18)}...</>}
-                                    </div>
-                                    <div className={styles.cardsellerborder}>
-                                        <div className={styles.cardsellerinsideborder}>
-                                        </div>
-                                    </div>
-                                    <div className={styles.cardpricesection}>
-                                        <div className='textprice'>
-                                            <span>A${item?.salePrice}</span>
-                                        </div>
-                                        <div className={styles.splitoffers}>
-                                            {item?.offerPercentag == 0 ? <></> : <span className='textpricedashedgreen'> <del>A${item?.actualPrice}</del></span>}
-                                            <span className='textpricedashedgreen ms-2'>
-                                                {item?.offerPercentag == 0 ? <></> : <>
-                                                    ({item?.offerPercentag} off)
-                                                </>}
-                                            </span>
-                                        </div>
 
-                                    </div>
-                                </div>
+                                                <Rate defaultValue={item?.quantityLeft} allowHalf style={{ color: "#54BE43", fontSize: "1.3rem", cursor: "pointer" }}
+                                                    tooltips={["Very Bad", "Bad", "Good", "Very Good", "Excellent"]}
+
+                                                    disabled
+
+                                                />
+
+                                                <div className={styles.brandname}>
+                                                    {item?.productName?.length <= 10 ? <>{item?.productName}</> : <>    {item?.productName.slice(0, 18)}...</>}
+                                                </div>
+
+                                                <div className='textgrey'>
+                                                    {item?.brandName?.length <= 18 ? <>{item?.brandName}</> : <>{item?.brandName.slice(0, 18)}...</>}
+                                                </div>
+                                                <div className={styles.cardsellerborder}>
+                                                    <div className={styles.cardsellerinsideborder}>
+                                                    </div>
+                                                </div>
+                                                <div className={styles.cardpricesection}>
+                                                    <div className='textprice'>
+                                                        <span>A${item?.salePrice}</span>
+                                                    </div>
+                                                    <div className={styles.splitoffers}>
+                                                        {item?.offerPercentag == 0 ? <></> : <span className='textpricedashedgreen'> <del>A${item?.actualPrice}</del></span>}
+                                                        <span className='textpricedashedgreen ms-2'>
+                                                            {item?.offerPercentag == 0 ? <></> : <>
+                                                                ({item?.offerPercentag} off)
+                                                            </>}
+                                                        </span>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })}</>}
                             </div>
-                        )
-                    })}</>}
-                </div>
 
 
                             <div>
