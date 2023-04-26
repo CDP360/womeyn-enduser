c0e207a613bb8286b13a8c1d77cdf4afca480b11
CDP360 <kalai@cdp360.com>
2023-04-27 03:34:53 +0530

    meta tages check in bug fixig so tired same error check

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -140,24 +140,7 @@ function Home() {
         <Fragment>
 
             <Head>
-
                 <title>Womeyn Home</title>
-                <meta name="title" content="Meta Tags — Preview, Edit and Generate" />
-                <meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
-
-
-                <meta property="og:type" content="@Womeyn" />
-                <meta property="og:url" content="https://metatags.io/" />
-                <meta property="og:title" content="kalaisurya" />
-                <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
-                <meta property="og:image" content="http://www.suntiros.com/wp-content/uploads/2018/01/Nazriya-Nazim-Sweet-Smile-Images.jpg" />
-
-
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://metatags.io/" />
-                <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate" />
-                <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
-                <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
             </Head>
 
 
