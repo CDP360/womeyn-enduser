cf93c50b625a250285643cc4316ca8912a4d03f2
CDP360 <kalai@cdp360.com>
2023-06-04 18:07:25 +0530

    token expire redirect bug fixig

--- src/config/Baseurl.js
+++ src/config/Baseurl.js
@@ -29,7 +29,7 @@ axios.interceptors.request.use(
         else {
             const token = localStorage.getItem("userToken");
             config.url = process.env.NEXT_PUBLIC_URL + config.url;
-           
+
             config.headers = {
                 ...config.headers,
                 "Content-Type": "application/json",
@@ -45,12 +45,31 @@ axios.interceptors.request.use(
 
 );
 
+
+
+
+
 axios.interceptors.response.use(
     function (response) {
         return response;
     },
     async function (error) {
-        return Promise.reject(error);
+        if (error.response.status === 401) {
+            await login_services.logout();
+            localStorage.removeItem("userid");
+            localStorage.removeItem("userToken");
+            localStorage.removeItem("userTokens");
+            localStorage.removeItem("whish");
+            localStorage.removeItem("user");
+            localStorage.removeItem("auth");
+            localStorage.removeItem("productid");
+            localStorage.removeItem('signupuser');
+            window.location.assign("/");
+            router.push("/");
+        } else {
+            return Promise.reject(error);
+        }
+
     }
 );
 
