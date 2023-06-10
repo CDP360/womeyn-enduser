490c5da0733c1b03adff99b7cc85be026811694f
CDP360 <kalai@cdp360.com>
2023-06-11 02:58:26 +0530

    service booking and datew expire check

--- src/services/servicewomeyn/service-womeyn.js
+++ src/services/servicewomeyn/service-womeyn.js
@@ -8,7 +8,7 @@ export function Serviceusers(current) {
     //     return err;
     // })
     return new Promise((resolve, reject) => {
-        instanceBaseurl.get(`/common/services?page=0&limit=${current}`).then(response => {
+        instanceBaseurl.get(`/common/services?page=0&limit="100"`).then(response => {
             resolve(response)
         }).catch(err => {
             reject(err)
