562ef7c0ae32a9d3edebc4ed4ea89438d9031d62
CDP360 <kalai@cdp360.com>
2023-07-03 16:36:35 +0530

    order oage bug fixig

--- src/components/checkout/components/checkoutsuccess/Checkoutsuccess.js
+++ src/components/checkout/components/checkoutsuccess/Checkoutsuccess.js
@@ -8,24 +8,32 @@ import Cookies from 'js-cookie';
 import ordercomplete from '../../../../assests/cart-logos/Order complete.gif';
 import Image from 'next/image';
 import { toast } from 'react-toastify';
+import LoaderLogo from './../../../loaderlogo/LoaderLogo';
 function Checkoutsuccess() {
     const history = useRouter();
     const searchParams = useSearchParams();
     const Transaction_id = searchParams.get('transaction_id');
     const paymentId_id = searchParams.get('paymentId');
     const PayerID_id = searchParams.get('PayerID');
+
+
     const [show, setShow] = useState(false);
-    const handleClose = () => {
-        setShow(false)
-        history.push("/profile/orders");
-    };
-    const handleShow = () => setShow(true);
+    // const handleClose = () => {
+    //     setShow(false)
+    //     history.push("/profile/orders");
+    // };
+    // const handleShow = () => setShow(true);
     useEffect(() => {
 
         if (Transaction_id) {
             handleShow();
+            setShow(true);
             CheckoutSuccessUpdate(Transaction_id).then((res) => {
+
+
                 if (res?.data?.message == "Order completed successfully") {
+
+                    setShow(false)
 
                     toast.success(res?.data?.message, {
                         position: "top-center",
@@ -39,15 +47,15 @@ function Checkoutsuccess() {
                     })
                     Cookies.remove("CartDatas");
                     Cookies.clear();
-                    setTimeout(() => {
 
-                        history.push("/profile/orders");
+                    history.push("/profile/orders");
 
-                    }, 800)
                 }
 
             }).catch((err) => {
                 console.log(err);
+                setShow(false);
+
             })
         }
 
@@ -55,6 +63,7 @@ function Checkoutsuccess() {
     useEffect(() => {
         if (paymentId_id, PayerID_id) {
             handleShow();
+            setShow(true)
             CheckoutSuccessUpdatePaypal(paymentId_id, PayerID_id).then((res) => {
                 if (res?.data?.message == "Order completed successfully") {
                     Cookies.remove("CartDatas");
@@ -70,19 +79,28 @@ function Checkoutsuccess() {
                         theme: "dark",
                     })
 
-                    setTimeout(() => {
+                    setShow(false);
 
-                        history.push("/profile/orders");
 
-                    }, 800)
+
+                    history.push("/profile/orders");
+
+
                 }
             }).catch((err) => {
                 console.log(err);
+                setShow(false);
+
             })
         }
     }, [PayerID_id, paymentId_id])
     return (
         <>
+
+            {show ? <>
+
+                <LoaderLogo />
+            </> : <></>}
             <div className={styles.success}>
                 <div>
                     <div className="mb-3">
